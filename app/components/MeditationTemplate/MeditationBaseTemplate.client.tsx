762770c2038017fac67bdfe278ef7bc51dcfc10d
Oleksandr Vakulin <vlipoq@gmail.com>
2026-07-02 15:38:27 +0200

    fix style for desktop

--- app/components/MeditationTemplate/MeditationBaseTemplate.client.tsx
+++ app/components/MeditationTemplate/MeditationBaseTemplate.client.tsx
@@ -28,89 +28,93 @@ const MeditationBaseTemplateClient = ({ day }: Prop) => {
           ))}
         </ul>
       </div>
-      <div className={css.blokMeditation}>
-        <h3 className={css.titleBlokMeditation}>Вплив на здоров’я</h3>
-        <p className={css.textBlokMeditation}>
-          Далі слід звернути увагу на первинні результати, які досягаються
-          шляхом правильної концентрації та розслаблення. Запускає позитивні
-          психофізичні зміни на рівні нервової системи та фізичного тіла,
-          оскільки безпосередньо це:
-        </p>
-        <ul className={css.listBlokMeditation}>
-          {meditationDay.health_effect.map((h) => (
-            <li key={h} className={css.itemListBlokMeditation}>
-              {" "}
-              - {h}
-            </li>
-          ))}
-        </ul>
+      <div className={css.sectionBloksMeditation}>
+        <div className={css.blokMeditation}>
+          <h3 className={css.titleBlokMeditation}>Вплив на здоров’я</h3>
+          <p className={css.textBlokMeditation}>
+            Далі слід звернути увагу на первинні результати, які досягаються
+            шляхом правильної концентрації та розслаблення. Запускає позитивні
+            психофізичні зміни на рівні нервової системи та фізичного тіла,
+            оскільки безпосередньо це:
+          </p>
+          <ul className={css.listBlokMeditation}>
+            {meditationDay.health_effect.map((h) => (
+              <li key={h} className={css.itemListBlokMeditation}>
+                {" "}
+                - {h}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={css.blokMeditation}>
+          <h3 className={css.titleBlokMeditation}>
+            Вплив на матеріальне благополуччя
+          </h3>
+          <p className={css.textBlokMeditation}>
+            Окрім суто внутрішніх змін, будь-яка глибока трансформація системно
+            впливає на якість взаємодії із зовнішнім світом. Тому стає природним
+            наслідком внутрішнього очищення, яке проекційно трансформує на
+            соціальні процеси та повсякденну зовнішню діяльність і:
+          </p>
+          <ul className={css.listBlokMeditation}>
+            {meditationDay.material_effect.map((m) => (
+              <li key={m} className={css.itemListBlokMeditation}>
+                {" "}
+                - {m}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className={css.blokMeditation}>
-        <h3 className={css.titleBlokMeditation}>
-          Вплив на матеріальне благополуччя
-        </h3>
-        <p className={css.textBlokMeditation}>
-          Окрім суто внутрішніх змін, будь-яка глибока трансформація системно
-          впливає на якість взаємодії із зовнішнім світом. Тому стає природним
-          наслідком внутрішнього очищення, яке проекційно трансформує на
-          соціальні процеси та повсякденну зовнішню діяльність і:
-        </p>
-        <ul className={css.listBlokMeditation}>
-          {meditationDay.material_effect.map((m) => (
-            <li key={m} className={css.itemListBlokMeditation}>
-              {" "}
-              - {m}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={css.blokMeditation}>
-        <h3 className={css.titleBlokMeditation}>Заборони та Рекомендації</h3>
-        <p className={css.textBlokMeditation}>
-          Окремим пунктом інструкції є суворе дотримання правил екологічності та
-          внутрішньої безпеки. Ігнорування перелічених обмежень є неприпустимим,
-          оскільки це може призвести до деструктивних станів або перевантаження
-          психіки, тому під час практики неприпустимим є:
-        </p>
-        <ul className={css.listBlokMeditation}>
-          {meditationDay.forbidden.map((f) => (
-            <li key={f} className={css.itemListBlokMeditation}>
-              {" "}
-              - {f}
-            </li>
-          ))}
-        </ul>
+      <div className={css.sectionBloksMeditation}>
+        <div className={css.blokMeditation}>
+          <h3 className={css.titleBlokMeditation}>Заборони та Рекомендації</h3>
+          <p className={css.textBlokMeditation}>
+            Окремим пунктом інструкції є суворе дотримання правил екологічності
+            та внутрішньої безпеки. Ігнорування перелічених обмежень є
+            неприпустимим, оскільки це може призвести до деструктивних станів
+            або перевантаження психіки, тому під час практики неприпустимим є:
+          </p>
+          <ul className={css.listBlokMeditation}>
+            {meditationDay.forbidden.map((f) => (
+              <li key={f} className={css.itemListBlokMeditation}>
+                {" "}
+                - {f}
+              </li>
+            ))}
+          </ul>
 
-        <p className={css.textBlokMeditation}>
-          Для підвищення ефективності та створення оптимальних умов під час
-          роботи обов'язково враховуйте супровідні рекомендації. Дотримання цих
-          правил допомагає правильно скерувати увагу та утримати потрібний стан,
-          тому для найкращого результату варто:
-        </p>
-        <ul>
-          {meditationDay.recommended.map((r) => (
-            <li key={r} className={css.itemListBlokMeditation}>
-              {" "}
-              - {r}
-            </li>
-          ))}
-        </ul>
-      </div>
+          <p className={css.textBlokMeditation}>
+            Для підвищення ефективності та створення оптимальних умов під час
+            роботи обов'язково враховуйте супровідні рекомендації. Дотримання
+            цих правил допомагає правильно скерувати увагу та утримати потрібний
+            стан, тому для найкращого результату варто:
+          </p>
+          <ul>
+            {meditationDay.recommended.map((r) => (
+              <li key={r} className={css.itemListBlokMeditation}>
+                {" "}
+                - {r}
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className={css.blokMeditation}>
-        <h3 className={css.titleBlokMeditation}>Чому саме так...</h3>
-        <p className={css.textBlokMeditation}>
-          Отже, для успішного виконання практики важливо усвідомити теоретичне
-          обґрунтування з необхідності інтегрувати стародавній досвід різних
-          культур для глибинного розуміння суті процесу, де в цей день:
-        </p>
-        <div className={css.listBlokMeditation}>
-          {meditationDay?.logic?.map((l) => (
-            <p key={l} className={css.textBlokMeditation}>
-              {" "}
-              {l}
-            </p>
-          ))}
+        <div className={css.blokMeditation}>
+          <h3 className={css.titleBlokMeditation}>Чому саме так...</h3>
+          <p className={css.textBlokMeditation}>
+            Отже, для успішного виконання практики важливо усвідомити теоретичне
+            обґрунтування з необхідності інтегрувати стародавній досвід різних
+            культур для глибинного розуміння суті процесу, де в цей день:
+          </p>
+          <div className={css.listBlokMeditation}>
+            {meditationDay?.logic?.map((l) => (
+              <p key={l} className={css.textBlokMeditation}>
+                {" "}
+                {l}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
 
